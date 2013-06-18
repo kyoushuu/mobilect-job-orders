178148d7bb9c1c26d47ec25019f9cd36b0ba54d7
Arnel A. Borja <kyoushuu@yahoo.com>
2013-06-18 23:38:29 +0800

    Add egg-list-box submodule

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,11 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 git submodule update --init --recursive
+
+cd egg-list-box
+sh autogen.sh --no-configure
+cd ..
+
 autoreconf -v --force --install
 intltoolize -f
 
