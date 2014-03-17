88e979e6956b8751fcd88463629569e2fdae8038
Arnel A. Borja <kyoushuu@yahoo.com>
2014-03-17 11:48:49 +0800

    Remove egg-list-box submodule

--- autogen.sh
+++ autogen.sh
@@ -7,11 +7,6 @@ test -z "$srcdir" && srcdir=.
 
 git submodule update --init --recursive
 
-cd egg-list-box
-sed s/GOBJECT_INTROSPECTION_REQUIRE/GOBJECT_INTROSPECTION_CHECK/ -i configure.ac
-sh autogen.sh --no-configure
-cd ..
-
 autoreconf -v --force --install
 intltoolize -f
 
